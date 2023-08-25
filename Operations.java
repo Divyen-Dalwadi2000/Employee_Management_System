697ba8941906e3513c2b81c304520a4ce2d7febd
Divyen <dalwadiddj2000@gmail.com>
2023-08-25 05:40:56 +0530

    Merge upstream master with resolved conflicts

---
+++ Operations.java
@@ -0,0 +1,13 @@
+package employeeManagementSystem;
+
+public interface Operations {
+  public void viewEmp();
+
+  public void updateEmp();
+
+  public void deleteEmp();
+
+  public void addEmp();
+
+  public void viewAllEmpDetails();
+}
